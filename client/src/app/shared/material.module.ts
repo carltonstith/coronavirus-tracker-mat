11a4e63a413c79de1661ba6520db3221d456cc07
Carlton Stith <carlton215@gmail.com>
2020-04-30 10:28:39 -0400

    added angular material

---
+++ client/src/app/shared/material.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    MatButtonModule,
+    MatIconModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatCheckboxModule,
+    MatIconModule
+  ]
+})
+export class MaterialModule { }
